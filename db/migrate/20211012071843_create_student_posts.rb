c25f1a117ca5b8856a1c556369ba7feaa229c609
keita170 <noinoi0565@gmail.com>
2021-10-12 08:07:47 +0000

    #2 投稿機能(comment-out)

--- db/migrate/20211012071843_create_student_posts.rb
+++ db/migrate/20211012071843_create_student_posts.rb
@@ -1,13 +1,12 @@
 class CreateStudentPosts < ActiveRecord::Migration[5.2]
   def change
     create_table :student_posts do |t|
-      
-      t.string :user_id
-      
-      t.string :title
-      t.text :body
-      t.integer :field
-      t.integer :status
+
+      t.string :user_id　#投稿にユーザーIDを結びつけたい為
+      t.string :title　#投稿のタイトル
+      t.text :body　#投稿の本文
+      t.integer :field　#投稿の分野
+      t.integer :status　#投稿のステータス(状況)
 
       t.timestamps
     end
