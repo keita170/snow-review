6ec0e7092d2576c9e9ca02808a71ef7384bcb26a
keita170 <noinoi0565@gmail.com>
2021-10-22 19:59:05 +0000

    fix 1to１(student_post/new)

--- db/migrate/20211012071843_create_student_posts.rb
+++ db/migrate/20211012071843_create_student_posts.rb
@@ -2,11 +2,11 @@ class CreateStudentPosts < ActiveRecord::Migration[5.2]
   def change
     create_table :student_posts do |t|
 
-      t.string :user_id　#投稿にユーザーIDを結びつけたい為
-      t.string :title　#投稿のタイトル
-      t.text :body　#投稿の本文
-      t.integer :field　#投稿の分野
-      t.integer :status　#投稿のステータス(状況)
+      t.string :user_id#投稿にユーザーIDを結びつけたい為
+      t.string :title#投稿のタイトル
+      t.text :body#投稿の本文
+      t.integer :field#投稿の分野
+      t.integer :status#投稿のステータス(状況)
 
       t.timestamps
     end
