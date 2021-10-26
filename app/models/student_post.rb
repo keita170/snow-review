9c3a022d24c4a90719837dc1c02bdd79ecffd09d
keita170 <noinoi0565@gmail.com>
2021-10-26 20:04:50 +0000

    #3.5いいね機能

--- app/models/student_post.rb
+++ app/models/student_post.rb
@@ -1,6 +1,11 @@
 class StudentPost < ApplicationRecord
   belongs_to :user
   has_many :student_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
   
   #enumを用いて数字で扱えるようにする
   enum field: { 選択肢１: 0, 選択肢２: 1 }
