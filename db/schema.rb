b20e36ebc878d7bc974b79f0393435a83eabb607
keita170 <noinoi0565@gmail.com>
2021-11-04 02:44:03 +0000

    #4 フォロー、アンフォロー機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_193825) do
+ActiveRecord::Schema.define(version: 2021_11_04_021255) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2021_10_26_193825) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "student_post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
